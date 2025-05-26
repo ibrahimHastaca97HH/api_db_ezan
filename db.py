788a7489d58fee9a598f05e2304782bcc9762d0f
ibrahimHastaca97HH <ibrahimhhastaca@gmail.com>
2025-05-26 11:25:57 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,15 +1,15 @@
-from sqlalchemy import create_engine, Column, String, Date, Table, MetaData
-from datetime import date
+from sqlalchemy import create_engine, MetaData, Table, Column, String, Date, Text
 
-engine = create_engine('sqlite:///vakitler.db')
-metadata = MetaData()
+engine = create_engine("sqlite:///vakitler.db", connect_args={"check_same_thread": False})
+meta = MetaData()
 
-vakitler = Table('vakitler', metadata,
-    Column('id', String, primary_key=True),  # il_ilce_tarih birleşimi
-    Column('il', String),
-    Column('ilce', String),
-    Column('tarih', Date),
-    Column('data', String),
+vakitler = Table(
+    "vakitler", meta,
+    Column("id", String, primary_key=True),
+    Column("il", String),
+    Column("ilce", String),
+    Column("tarih", Date),
+    Column("data", Text),
 )
 
-metadata.create_all(engine)
+meta.create_all(engine)
